bb3702203e4dfde843b6ce7798a0e380dfd3e0f2
720387 <720387@alu.murciaeduca.es>
2024-03-07 12:24:51 +0100

    Cambiado el nombre de la clase Contacto por Persona.

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -6,11 +6,11 @@ import java.util.List;
 /**
  * Comentario JavaDoc en la clase Contacto
  */
-class Contacto {
+class Persona {
     private String name;
     private List<String> phones;
 
-    public Contacto(String name, String phone) {
+    public Persona(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
